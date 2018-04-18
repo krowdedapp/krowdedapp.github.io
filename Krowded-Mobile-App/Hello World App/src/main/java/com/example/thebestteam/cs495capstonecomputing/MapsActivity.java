1421e310ce5376a6c671d4ca09983afd38aec327
Nick Chadwick <nicholasandrewchadwick@gmail.com>
2018-04-17 21:11:57 -0500

    changed login button on map to onClick function

--- Krowded-Mobile-App/Hello World App/src/main/java/com/example/thebestteam/cs495capstonecomputing/MapsActivity.java
+++ Krowded-Mobile-App/Hello World App/src/main/java/com/example/thebestteam/cs495capstonecomputing/MapsActivity.java
@@ -115,17 +115,7 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
 
         // Initialize user
         user = LoginActivity.user;
-
-        final Button button = findViewById(R.id.btnLogin);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                if (user != null && user.isLoggedIn()) {
-                    startActivity(new Intent(MapsActivity.this, ProfileActivity.class));
-                } else {
-                    startActivity(new Intent(MapsActivity.this, LoginActivity.class));
-                }
-            }
-        });
+        
     }
 
         @Override
@@ -366,5 +356,13 @@ public class MapsActivity extends FragmentActivity implements LocationListener, 
     @Override
     public void onStatusChanged(String provider, int status, Bundle extras) {
         // TODO Auto-generated method stub
+    }
+
+    public void loginButton(View view) {
+        if (user != null && user.isLoggedIn()) {
+            startActivity(new Intent(MapsActivity.this, ProfileActivity.class));
+        } else {
+            startActivity(new Intent(MapsActivity.this, LoginActivity.class));
+        }
     }
 }
