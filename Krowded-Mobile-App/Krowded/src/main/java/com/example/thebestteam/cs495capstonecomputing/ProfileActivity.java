5c19f53438fdce2350931af7aef133f47b18202c
ksfoster1 <ksfoster1@crimson.ua.edu>
2018-04-26 21:08:05 -0500

    add marker colors started making legends

--- Krowded-Mobile-App/Krowded/src/main/java/com/example/thebestteam/cs495capstonecomputing/ProfileActivity.java
+++ Krowded-Mobile-App/Krowded/src/main/java/com/example/thebestteam/cs495capstonecomputing/ProfileActivity.java
@@ -2,18 +2,13 @@ package com.example.thebestteam.cs495capstonecomputing;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -22,8 +17,6 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-
-import static com.example.thebestteam.cs495capstonecomputing.MainActivity.*;
 
 public class ProfileActivity extends AppCompatActivity {
     User user;
@@ -52,13 +45,9 @@ public class ProfileActivity extends AppCompatActivity {
 
         DatabaseReference mRoot = FirebaseDatabase.getInstance().getReference();
 
-
         mRoot.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-
-
-
 
                 String cleanEmail = user.getEmail().replace(".","");
 
